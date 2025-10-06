2b79c7307a8bc51a03b004497102c4dbbf42bb4f
dan <danmgarcia@hotmail.com>
2025-10-06 20:57:26 +0200

    introducido gimnasio

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,17 @@
 const burg = document.getElementById('burg');
-const cierre = document.getElementById('cierre')
-const indice = document.getElementById('indice')
-// console.log(indice);
+const cierre = document.getElementById('cierre');
+const indice = document.getElementById('indice');
+const websDialog = document.getElementById('webs-dialog');
+const wrapper = document.querySelector('.wrapper');
+const cards = document.querySelectorAll('.card');
+
+cards.forEach((card => {
+    card.addEventListener('click', function(){
+        if(card.hasAttribute('onclick')){
+            wrapper.children[0].attributes[0].nodeValue = card.children[1].attributes[0].nodeValue
+        }
+    })
+}))
 
 burg.addEventListener('click', ()=> {
     burg.classList = ('offburg');
@@ -16,4 +26,18 @@ cierre.addEventListener('click', ()=> {
     cierre.classList = ('offcierre');
     burg.classList = ('');
     indice.style.transform = 'translateY(-100%)';
+})
+
+
+function mostrarDialog(){
+    websDialog.showModal()
+}
+function cerrarDialog(){
+    websDialog.close()
+}
+
+websDialog.addEventListener('click', (e) => {
+    if(!wrapper.contains(e.target)){
+        cerrarDialog();
+    }
 })
